a90bfa0dc32b27fdd2cb5761f864c9bb75bd05a7
Abena-Dumfeh <serwahmaame19@gmail.com>
2026-01-09 14:40:48 +0900

    Backend focused project

--- flask_app/config (Copy).py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'a-very-secret-key-for-session'  # 🔐 Used for session cookies
-
-    # If you want to later store config for DB, Fabric, IPFS, etc., you can add here
--- flask_app/utils/ipfs_client.py
+++
@@ -1,4 +0,0 @@
-import ipfshttpclient
-
-def get_ipfs_client():
-    return ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
